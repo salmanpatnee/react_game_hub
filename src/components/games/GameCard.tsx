fd85af1819b924ca282b7dbdfac82035001768c4
salmanpatnee <salmanpatni92@gmail.com>
2025-06-05 17:03:57 +0500

    Refactor: Remove duplicated styles

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -2,6 +2,7 @@ import type { Game } from "@/hooks/useGame";
 import { Card, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformList from "./PlatformList";
 import CriticScore from "./CriticScore";
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
@@ -9,16 +10,19 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
-      <Card.Body>
-        <Heading fontSize="2xl" >{game.name}</Heading>
-        <HStack justifyContent="space-between" marginTop={2}>
-          <PlatformList platforms={game.parent_platforms.map(p => p.platform)} />
-          <CriticScore score={game.metacritic}/>
-        </HStack>
-      </Card.Body>
-    </Card.Root>
+    <GameCardContainer>
+      <Card.Root>
+        <Image src={game.background_image} />
+        <Card.Body>
+          <Heading fontSize="2xl" >{game.name}</Heading>
+          <HStack justifyContent="space-between" marginTop={2}>
+            <PlatformList platforms={game.parent_platforms.map(p => p.platform)} />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </Card.Body>
+      </Card.Root>
+    </GameCardContainer>
+
   )
 }
 
--- src/components/games/GameCardSkeleton.tsx
+++ src/components/games/GameCardSkeleton.tsx
@@ -1,13 +1,16 @@
 import { Card, Skeleton, SkeletonText } from '@chakra-ui/react'
+import GameCardContainer from './GameCardContainer'
 
 const GameCardSkeleton = () => {
     return (
-        <Card.Root borderRadius={10} overflow="hidden" width="300px">
-            <Skeleton height="200px" width="100%" />
-            <Card.Body>
-                <SkeletonText noOfLines={2} width="80%" />
-            </Card.Body>
-        </Card.Root>
+        <GameCardContainer>
+            <Card.Root>
+                <Skeleton height="200px" width="100%" />
+                <Card.Body>
+                    <SkeletonText noOfLines={2} width="80%" />
+                </Card.Body>
+            </Card.Root>
+        </GameCardContainer>
     )
 }
 
